10ff0e5219c28628283f14f126a175217ede8e15
Yagyos <Yagyos@users.noreply.github.com>
2019-10-16 18:45:54 +0900

    いろいろ分けてる

---
+++ ConsumptionTaxCalculation/ConsumptionTaxCalc.vb
@@ -0,0 +1,106 @@
+﻿Option Explicit On '型宣言を強制
+Option Strict On 'タイプ変換を厳密に
+
+Public Module ConsumptionTaxCalc
+
+    Structure DataSet
+        Dim OriginalAmount As Decimal '算出元の金額
+        Dim TaxRate As Decimal '税率 8%→[8] 10%→[10]
+        Dim TaxRound As RoundCalc.RoundType '丸め種類
+        Dim RoundDigits As Integer '丸め桁数 0:小数点以下丸め(12.3→12) 1:(12→10) -1:(1.23→1.2)
+        Dim OutsideTaxAmount As Decimal '外税税額
+        Dim InsideTaxAmount As Decimal '内税税額
+        Dim InsideNetAmount As Decimal '内税正味金額
+    End Structure
+
+    ''' <summary>
+    ''' 税計算実施
+    ''' </summary>
+    ''' <param name="ConTaxCalData"></param>
+    ''' <remarks></remarks>
+    Public Sub Calculation(ByRef ConTaxCalData As DataSet)
+
+        '外税税額算出
+        Call CalculationOutside(ConTaxCalData)
+
+        '内税税額算出
+        Call CalculationInside(ConTaxCalData)
+
+    End Sub
+
+    ''' <summary>
+    ''' 外税税額算出 丸め処理も実施
+    ''' </summary>
+    ''' <param name="ConTaxCalData"></param>
+    ''' <returns>外税税額</returns>
+    ''' <remarks></remarks>
+    Public Function CalculationOutside(ByRef ConTaxCalData As DataSet) As Decimal
+
+        Dim TaxAmount As Decimal
+
+        '外税税額算出
+        TaxAmount = CalculationOutsideTax(ConTaxCalData.OriginalAmount, ConTaxCalData.TaxRate)
+        ConTaxCalData.OutsideTaxAmount = RoundCalc.Calculation(TaxAmount, ConTaxCalData.TaxRound, ConTaxCalData.RoundDigits)
+
+        Return ConTaxCalData.OutsideTaxAmount
+    End Function
+
+    ''' <summary>
+    ''' 外税税額算出 丸め処理前
+    ''' </summary>
+    ''' <param name="OutsideNetAmount"></param>
+    ''' <param name="TaxRate"></param>
+    ''' <returns>外税税額</returns>
+    ''' <remarks></remarks>
+    Public Function CalculationOutsideTax(ByVal OutsideNetAmount As Decimal, ByVal TaxRate As Decimal) As Decimal
+
+        '[8%]を[0.08]に
+        '[10%]を[0.1]に
+        TaxRate = TaxRate / 100
+
+        '丸め前の税額を返す
+        Return OutsideNetAmount * TaxRate
+    End Function
+
+    ''' <summary>
+    ''' 内税税額算出 丸め処理も実施 税別金額はInsideNetAmount
+    ''' </summary>
+    ''' <param name="ConTaxCalData"></param>
+    ''' <returns>内税税額 InsideTaxAmount:内税税額 InsideNetAmount:税別金額</returns>
+    ''' <remarks></remarks>
+    Public Function CalculationInside(ByRef ConTaxCalData As DataSet) As Decimal
+
+        Dim TaxAmount As Decimal
+
+        '内税税額算出
+        TaxAmount = CalculationInsideTax(ConTaxCalData.OriginalAmount, ConTaxCalData.TaxRate)
+        ConTaxCalData.InsideTaxAmount = RoundCalc.Calculation(TaxAmount, ConTaxCalData.TaxRound, ConTaxCalData.RoundDigits)
+
+        '内税正味金額算出
+        ConTaxCalData.InsideNetAmount = ConTaxCalData.OriginalAmount - ConTaxCalData.InsideTaxAmount
+
+        Return ConTaxCalData.InsideTaxAmount
+    End Function
+
+    ''' <summary>
+    ''' 内税税額算出 丸め処理前
+    ''' </summary>
+    ''' <param name="InsideNetAmount"></param>
+    ''' <param name="TaxRate"></param>
+    ''' <returns>内税税額</returns>
+    ''' <remarks></remarks>
+    Public Function CalculationInsideTax(ByVal InsideNetAmount As Decimal, ByVal TaxRate As Decimal) As Decimal
+
+        '[8%]を[1.08]に
+        '[10%]を[1.1]に
+        TaxRate = (100 + TaxRate) / 100
+
+        '内税正味金額算出
+        Dim OutsideNetAmount As Decimal
+        OutsideNetAmount = InsideNetAmount / TaxRate
+
+        '丸め前の税額を返す
+        Return InsideNetAmount - OutsideNetAmount
+    End Function
+
+End Module
---
+++ ConsumptionTaxCalculation/RoundCalc.vb
@@ -0,0 +1,100 @@
+﻿Option Explicit On '型宣言を強制
+Option Strict On 'タイプ変換を厳密に
+
+Public Module RoundCalc
+
+    ''' <summary>
+    ''' 丸め種類 off:四捨五入 down:切り捨て up:切り上げ
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Enum RoundType
+        Round_off  '四捨五入
+        Round_down '切り捨て
+        Round_up   '切り上げ
+    End Enum
+
+    ''' <summary>
+    ''' 丸め実施(小数点以下を丸め)
+    ''' </summary>
+    ''' <param name="TaxAmount">丸め元の金額</param>
+    ''' <param name="TaxRound"></param>
+    ''' <returns>丸められた金額</returns>
+    ''' <remarks></remarks>
+    Public Function Calculation(ByVal TaxAmount As Decimal, ByVal TaxRound As RoundType) As Decimal
+        Return Calculation(TaxAmount, TaxRound, 0)
+    End Function
+
+    ''' <summary>
+    ''' 丸め実施
+    ''' </summary>
+    ''' <param name="TaxAmount">丸め元の金額</param>
+    ''' <param name="TaxRound"></param>
+    ''' <param name="RoundDigits">丸め桁数 0:小数点以下丸め(12.3→12) 1:(12→10) -1:(1.23→1.2)</param>
+    ''' <returns>丸められた金額</returns>
+    ''' <remarks></remarks>
+    Public Function Calculation(ByVal TaxAmount As Decimal, ByVal TaxRound As RoundType, ByVal RoundDigits As Integer) As Decimal
+
+        '負や正の無限大への丸めや
+        '銀行丸めがあるので注意
+
+        '無限大方向丸め対策として
+        'マイナスはプラスとして丸め処理する
+        Dim Minus As Decimal
+        If TaxAmount < 0 Then
+            Minus = -1
+            TaxAmount = TaxAmount * -1
+        Else
+            Minus = 1
+        End If
+
+        '丸め計算用に桁数を合わせる
+        TaxAmount = TaxAmount / CDec(10 ^ RoundDigits)
+
+        Select Case TaxRound
+            Case RoundType.Round_down
+                '切り捨て
+                TaxAmount = Math.Truncate(TaxAmount)
+            Case RoundType.Round_up
+                '切り上げ
+                TaxAmount = Math.Ceiling(TaxAmount)
+            Case Else
+                '四捨五入
+
+                '銀行丸めに対処するため
+                '0.5を足して切り捨てを実施
+                TaxAmount = TaxAmount + 0.5D
+                TaxAmount = Math.Truncate(TaxAmount)
+
+        End Select
+
+        '合わせた桁数を戻す
+        TaxAmount = TaxAmount * CDec(10 ^ RoundDigits)
+
+        Return TaxAmount * Minus
+    End Function
+
+    'セルフチェック
+    Public Sub SelfCheck()
+
+        If Calculation(123.456D, RoundType.Round_off) <> 123 Then
+            Debug.Assert(False)
+        End If
+        If Calculation(123.56D, RoundType.Round_off) <> 124 Then
+            Debug.Assert(False)
+        End If
+        If Calculation(123.456D, RoundType.Round_up) <> 124 Then
+            Debug.Assert(False)
+        End If
+        If Calculation(123.56D, RoundType.Round_down) <> 123 Then
+            Debug.Assert(False)
+        End If
+        If Calculation(555D, RoundType.Round_off, 1) <> 560 Then
+            Debug.Assert(False)
+        End If
+        If Calculation(555.555D, RoundType.Round_off, -1) <> 555.6 Then
+            Debug.Assert(False)
+        End If
+
+    End Sub
+
+End Module
